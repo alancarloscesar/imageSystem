97d606ae0304a9657e29e0cfc84fa3d4614769ed
alancesar <alancarloscesar@gmail.com>
2023-01-17 15:05:05 -0300

    finalizando user, com testes e docker

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const UserController = require('../src/controllers/UserController')
 
+
 router.get('/', (req, res) => res.send("Hello Word!"))
 router.post('/user/create', UserController.store)
+router.delete('/user/delete/:name', UserController.destroy)
+router.post("/auth", UserController.jwtTokenGenerate)
 
 module.exports = router;
